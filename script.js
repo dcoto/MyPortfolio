77c896a220dfd8383f7e0980b402d7cf19fb2546
David <131292839+dcoto@users.noreply.github.com>
2023-10-04 04:24:17 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
+var menu = document.querySelector(".menu-links");
+var icon = document.querySelector(".hamburger-icon");
+var content = document.getElementById("hide");
+
+
+
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
-}
-
-
-
+  content.classList.toggle("hide");
   
-
+}
+  
 window.addEventListener('scroll', reveal);
 
 function reveal(){
   var reveals = document.querySelectorAll('.reveal');
-  var header = document.querySelector('header');
+  
 
   for(var i= 0; i< reveals.length; i++){
     
@@ -62,3 +64,31 @@ function preloadImages(){
 preloadImages("./assets/coding.gif", "./assets/toad.png", "./assets/mario.png");
 
 
+let lastScrollTop = window.scrollY || document.documentElement.scrollTop;
+var header = document.querySelector('header');
+window.addEventListener(
+  'scroll',
+  function handleScroll() {
+    const scrollTopPosition =
+      window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTopPosition > lastScrollTop) {
+      console.log('scrolling down');
+      header.classList.add('head-scrolled');
+      menu.classList.remove("open");
+      icon.classList.remove("open");
+      content.classList.remove("hide");
+      
+      
+      
+    } else if (scrollTopPosition < lastScrollTop) {
+      console.log('scrolling up');
+     
+      header.classList.remove('head-scrolled');
+      
+    }
+    lastScrollTop =
+      scrollTopPosition <= 0 ? 0 : scrollTopPosition;
+  },
+  false,
+);
