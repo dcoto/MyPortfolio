74e45874f058a854a87ca4cc1da1141ffed5287c
David <131292839+dcoto@users.noreply.github.com>
2023-10-03 19:15:17 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ window.addEventListener('scroll', reveal);
 
 function reveal(){
   var reveals = document.querySelectorAll('.reveal');
+  var header = document.querySelector('header');
 
   for(var i= 0; i< reveals.length; i++){
     
@@ -23,11 +24,41 @@ function reveal(){
     if(revealTop < windowHeight - revealPoint){
       reveals[i].classList.add('active');
     }
+
     else{
       reveals[i].classList.remove('active');
     }
 
   }
 
+}
+
+document.addEventListener('scroll', () => {
+  var header = document.querySelector('header');
+
+if(window.scrollY > 0){
+  header.classList.add('head-scrolled');
+}
+  else{
+    header.classList.remove('head-scrolled');
+  }
+})
+
+
+var images = new Array();
+
+function preloadImages(){
+
+    for (i=0; i < preloadImages.arguments.length; i++){
+
+         images[i] = new Image();
+
+        images[i].src = preloadImages.arguments[i];
+
+    }
 
 }
+
+preloadImages("./assets/coding.gif", "./assets/toad.png", "./assets/mario.png");
+
+
